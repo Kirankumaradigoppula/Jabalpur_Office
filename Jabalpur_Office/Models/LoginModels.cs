854d83ea0cef745e03660d04af0e9d122f3f3775
Kirankumaradigoppula <adigoppula.kkumar@gmail.com>
2025-07-16 18:17:39 +0530

    New Model Added -

---
+++ Jabalpur_Office/Models/LoginModels.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jabalpur_Office.Models
+{
+    public class LoginSeatRequest
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string NAME { get;set;}
+
+        [Required(ErrorMessage = "Mobile number is required.")]
+        public string MOBNO { get; set; }
+    }
+}
