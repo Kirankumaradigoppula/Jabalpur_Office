8a95ea153549054ea96291fd3b51b4c773e1213d
Kirankumaradigoppula <adigoppula.kkumar@gmail.com>
2025-07-21 09:23:51 +0530

    New Model ValidateUserRequest And OtpRequest - Added

--- Jabalpur_Office/Models/LoginModels.cs
+++ Jabalpur_Office/Models/LoginModels.cs
@@ -5,9 +5,30 @@ namespace Jabalpur_Office.Models
     public class LoginSeatRequest
     {
         [Required(ErrorMessage = "Name is required.")]
-        public string NAME { get;set;}
+        public string NAME { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Mobile number is required.")]
-        public string MOBNO { get; set; }
+        public string MOBNO { get; set; } = string.Empty;
+    }
+
+    public class ValidateUserRequest
+    {
+      
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string USERNAME { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password number is required.")]
+        public string PASSWORD { get; set; } = string.Empty;
+    }
+
+    public class OtpRequest
+    {
+        [Required(ErrorMessage = "Mobile Number is required")]
+        public string MOBNO { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Role Number is required")]
+        public string ROLE { get; set; } = string.Empty;
     }
 }
