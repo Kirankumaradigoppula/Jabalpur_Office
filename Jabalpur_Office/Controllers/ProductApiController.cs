eea9b2209ae5edeaa8f8a464e9a8baa8b39ef477
Kirankumaradigoppula <adigoppula.kkumar@gmail.com>
2025-10-29 08:56:38 +0530

    New Api Added - CrudVisitorDetails ,GetVisitorWorkDetailsList

--- Jabalpur_Office/Controllers/ProductApiController.cs
+++ Jabalpur_Office/Controllers/ProductApiController.cs
@@ -5574,6 +5574,286 @@ namespace Jabalpur_Office.Controllers
             }, nameof(GetVisitorMasterDetails), out _, skipTokenCheck: false));
         }
 
+        [HttpPost("CrudVisitorDetails")]
+        public IActionResult CrudVisitorDetails([FromForm] string input, [FromForm] List<IFormFile> VIS_INWARD_FILE, [FromForm] string VIS_WEBCAM_FILE)
+        {
+            return Ok(ExecuteWithHandling(() =>
+            {
+                var (outObj, rawData) = PrepareWrapperAndData<WrapperCrudObjectData>(
+                   string.IsNullOrEmpty(input) ? new { } : ApiHelper.ToObject(input) // deserialize JSON string
+
+                 );
+
+                var data = ApiHelper.ToObjectDictionary(rawData); // Dictionary<string, object>
+                var filterKeys = ApiHelper.GetFilteredKeys(data);
+                string pInwardFilePath = string.Empty;
+                string pWebCamFilePath = string.Empty;
+                string flag = data.ContainsKey("FLAG") ? data["FLAG"]?.ToString() ?? "" : "";
+
+                if (flag =="DELETE")
+                {
+                    string pInwardQry = @"SELECT VIS_INWARD_FILE_PATH FROM VISITOR  WHERE MP_SEAT_ID = @MP_SEAT_ID AND VIS_SRNO=@VIS_SRNO";
+                    // Get old MEDIA_DATE from DB (before update)
+                     pInwardFilePath = Convert.ToString(
+                         _core.ExecScalarText(
+                              pInwardQry,
+                               new[] {
+                                new SqlParameter("@MP_SEAT_ID", pJWT_MP_SEAT_ID),
+                                new SqlParameter("@VIS_SRNO", data["VIS_SRNO"]?.ToString())
+
+                               }
+                          )
+                       );
+                    string pWebCamQry = @"SELECT VIS_WEBCAM_FILE_PATH FROM VISITOR  WHERE MP_SEAT_ID = @MP_SEAT_ID AND VIS_SRNO=@VIS_SRNO";
+                    // Get old MEDIA_DATE from DB (before update)
+                    pWebCamFilePath = Convert.ToString(
+                        _core.ExecScalarText(
+                             pWebCamQry,
+                              new[] {
+                                new SqlParameter("@MP_SEAT_ID", pJWT_MP_SEAT_ID),
+                                new SqlParameter("@VIS_SRNO", data["VIS_SRNO"]?.ToString())
+
+                              }
+                         )
+                      );
+
+                }
+
+                // Step 2: Build SQL parameters (advanced dynamic approach)
+                var (paramList, pStatus, pMsg, pRetId) = SqlParamBuilderWithAdvancedCrud.BuildAdvanced(
+                    data: data,
+                    keys: filterKeys,
+                    mpSeatId: pJWT_MP_SEAT_ID,
+                    userId: pJWT_USERID,
+                    includeRetId: true
+                );
+
+                DataTable dt = _core.ExecProcDt("ReactCrudVisitorDetails", paramList.ToArray());
+                SetOutputParamsWithRetId(pStatus, pMsg, pRetId, outObj);
+
+                if (outObj.StatusCode == 200 )
+                {
+                    
+                    if (flag == "SAVE" || flag == "UPDATE")
+                    {
+
+                        if (flag == "SAVE")
+                        {
+                            //Here MaxSrno Nothing but pRetId
+                            string pREASON_SHORT_NAME = string.Empty;
+                            string pQry = @"SELECT  ISNULL(MAX(REASON_MAX_SRNO),0)+1 REASON_MAX,REASON_SHORT_NAME FROM REASON_MASTER  WHERE  REASON_MAS_ID=@VIS_REASON GROUP BY REASON_SHORT_NAME";
+                            DataTable dt2 = _core.ExecProcDt(
+                                    pQry,
+                                    new[] {
+                                 new SqlParameter("@VIS_REASON", data["VIS_REASON"]?.ToString())
+                                    }
+                                );
+
+                            if (dt2 != null && dt2.Rows.Count > 0)
+                            {
+                                outObj.ExtraData["REASON_MAXSRNO"] = Convert.ToString(dt2.Rows[0]["REASON_SHORT_NAME"]) + "/" + Convert.ToString(dt2.Rows[0]["REASON_MAX"]); ;
+                            }
+                            string pVIS_INWARD_LETTER_NO = string.Empty;
+                            string pInwardIdQry = @"SELECT VIS_INWARD_LETTER_NO FROM VISITOR  WHERE MP_SEAT_ID = @MP_SEAT_ID AND VIS_SRNO=@VIS_SRNO";
+                            // Get old MEDIA_DATE from DB (before update)
+                            pVIS_INWARD_LETTER_NO = Convert.ToString(
+                                _core.ExecScalarText(
+                                     pInwardIdQry, 
+                                      new[] {
+                                      new SqlParameter("@MP_SEAT_ID", pJWT_MP_SEAT_ID),
+                                      new SqlParameter("@VIS_SRNO",  outObj.RetID.ToString())
+
+                                      }
+                                 )
+                              );
+                            outObj.ExtraData["VIS_INWARD_LETTER_NO"] = pVIS_INWARD_LETTER_NO;
+
+                        }
+                        if (VIS_INWARD_FILE != null && VIS_INWARD_FILE.Count > 0)
+                        {
+
+                            if (VIS_INWARD_FILE.Count > 1)
+                            {
+                                outObj.StatusCode = 500;
+                                outObj.Message = "You can upload a maximum of One files.";
+                                outObj.LoginStatus = pJWT_LOGIN_NAME;
+                                return outObj;
+                            }
+                            var file = VIS_INWARD_FILE.First();
+
+                            string[] allowedExt = new[] { ".jpg", ".jpeg", ".png", ".pdf", ".doc",  ".docx",  ".xls", ".xlsx" };
+                            string ext = Path.GetExtension(file.FileName)?.ToLower() ?? "";
+                            if (!allowedExt.Contains(ext))
+                            {
+                                outObj.StatusCode = 500;
+                                outObj.Message = "Only jpg, png, doc, docx, xls, xlxs files are allowed.";
+                                outObj.LoginStatus = pJWT_LOGIN_NAME;
+                                return outObj;
+                            }
+
+                            // Validate file size (max 2MB)
+                            double fileSizeInMB = file.Length / 1024.0 / 1024.0;
+                            if (fileSizeInMB > 2)
+                            {
+                                outObj.StatusCode = 400;
+                                outObj.Message = $"File '{file.FileName}' exceeds 2MB limit.";
+                                outObj.LoginStatus = pJWT_LOGIN_NAME;
+                                return outObj;
+                            }
+
+                            // Prepare storage paths
+                            string storageRoot = _settings.BasePath;
+                            string baseFolder = Path.Combine("image", $"MP_{pJWT_MP_SEAT_ID}", "visitorAttachedFiles");
+                            string finalFolder = Path.Combine(storageRoot, baseFolder);
+                            if (!Directory.Exists(finalFolder))
+                            {
+                                Directory.CreateDirectory(finalFolder);
+                            }
+                            string FileName = $"{pJWT_MP_SEAT_ID}_{outObj.RetID}{ext}"; 
+                            string relativePath = Path.Combine(baseFolder, FileName).Replace("\\", "/");
+                            string fullPath = Path.Combine(finalFolder, FileName);
+                            // ✅ If file already exists, delete it before saving
+                            if (System.IO.File.Exists(fullPath))
+                            {
+                                System.IO.File.Delete(fullPath);
+                            }
+
+                            // ✅ Save the uploaded file to server
+                            using (var stream = new FileStream(fullPath, FileMode.Create))
+                            {
+                                //await file.CopyToAsync(stream); // file is IFormFile
+                                file.CopyTo(stream); // sync version
+                            }
+
+                            string vQryUpdateStatus = $"UPDATE VISITOR SET VIS_INWARD_DOCUMENT='" + FileName + "' ,VIS_INWARD_FILE_PATH='" + relativePath + "' WHERE MP_SEAT_ID='" + pJWT_MP_SEAT_ID + "' AND VIS_SRNO='" + outObj.RetID + "' ";
+                            _core.ExecNonQuery(vQryUpdateStatus);
+
+                            
+                        }
+                        if (!string.IsNullOrEmpty(VIS_WEBCAM_FILE))
+                        {
+
+                            //string[] base64 = VIS_WEBCAM_FILE.Split(new char[] { ',' });
+                            var base64 = VIS_WEBCAM_FILE.Split(',');
+                            if (base64.Length > 1)
+                            {
+                                string ImgStr = base64[1];
+                                byte[] imageBytes = Convert.FromBase64String(ImgStr);
+                                // Prepare storage paths
+                                string storageRoot = _settings.BasePath;
+                                string baseFolder = Path.Combine("image", $"MP_{pJWT_MP_SEAT_ID}", "WebCamImg");
+                                string finalFolder = Path.Combine(storageRoot, baseFolder);
+                                if (!Directory.Exists(finalFolder))
+                                {
+                                    Directory.CreateDirectory(finalFolder);
+                                }
+                                string FileName = $"{pJWT_MP_SEAT_ID}_WebCam_{outObj.RetID}.jpg";
+                                string relativePath = Path.Combine(baseFolder, FileName).Replace("\\", "/");
+                                string fullPath = Path.Combine(finalFolder, FileName);
+                                // ✅ If file already exists, delete it before saving
+                                if (System.IO.File.Exists(fullPath))
+                                {
+                                    System.IO.File.Delete(fullPath);
+                                }
+                                // ✅ Save decoded webcam image
+                                //File.WriteAllBytes(fullPath, imageBytes);
+                                System.IO.File.WriteAllBytes(fullPath, imageBytes);
+
+                                string vQryUpdateStatus = $"UPDATE VISITOR SET VIS_WEBCAM_IMG='" + FileName + "' ,VIS_WEBCAM_FILE_PATH='" + relativePath + "' WHERE MP_SEAT_ID='" + pJWT_MP_SEAT_ID + "' AND VIS_SRNO='" + outObj.RetID + "' ";
+                                _core.ExecNonQuery(vQryUpdateStatus);
+
+                            }
+                        }
+                    }
+                    if (flag == "DELETE")
+                    {
+                        
+                        if (!string.IsNullOrEmpty(pInwardFilePath))
+                        {
+                            string inwardFullPath = Path.Combine(_settings.BasePath, pInwardFilePath.Replace("/", "\\"));
+                            if (System.IO.File.Exists(inwardFullPath))
+                            {
+                                System.IO.File.Delete(inwardFullPath);
+                                string parentFolder = Path.GetDirectoryName(inwardFullPath);
+                                if (Directory.Exists(parentFolder) && !Directory.EnumerateFileSystemEntries(parentFolder).Any())
+                                    Directory.Delete(parentFolder, true);
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(pWebCamFilePath))
+                        {
+                            string webcamFullPath = Path.Combine(_settings.BasePath, pWebCamFilePath.Replace("/", "\\"));
+                            if (System.IO.File.Exists(webcamFullPath))
+                            {
+                                System.IO.File.Delete(webcamFullPath);
+                                string parentFolder = Path.GetDirectoryName(webcamFullPath);
+                                if (Directory.Exists(parentFolder) && !Directory.EnumerateFileSystemEntries(parentFolder).Any())
+                                    Directory.Delete(parentFolder, true);
+                            }
+                        }
+                        
+
+                    }
+                }
+                return outObj;
+
+            }, nameof(CrudVisitorDetails), out _, skipTokenCheck: false));
+
+        }
+
+
+        [HttpPost("GetVisitorWorkDetailsList")]
+        public IActionResult GetVisitorWorkDetailsList([FromBody] object input)
+        {
+            return Ok(ExecuteWithHandling(() =>
+            {
+                var (outObj, rawData) = PrepareWrapperAndData<WrapperListData>(input ?? new { });
+
+                var data = ApiHelper.ToObjectDictionary(rawData); // Dictionary<string, object>
+                var filterKeys = ApiHelper.GetFilteredKeys(data);
+
+                // Extract search, paging
+                var (pSearch, pageIndex, pageSize) = ApiHelper.GetSearchAndPagingObject(data);
+
+                // Step 2: Build SQL parameters (advanced dynamic approach)
+                var (paramList, pStatus, pMsg, pTotalCount, pWhere) = SqlParamBuilderWithAdvanced.BuildAdvanced(
+                    data: data,
+                    keys: filterKeys,
+                    mpSeatId: pJWT_MP_SEAT_ID,
+                    includeTotalCount: true,
+                    includeWhere: true,
+                    pageIndex: pageIndex,
+                    pageSize: pageSize
+                );
+
+                var pTotalAmount = new SqlParameter("@pTotalAmount", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                paramList.Add(pTotalAmount);
+
+                DataTable dt = _core.ExecProcDt("ReactVisitorWorkDetails", paramList.ToArray());
+                ApiHelper.SetDataTableListOutput(dt, outObj);
+                SetOutput(pStatus, pMsg, outObj);
+
+                // Read @pTotalAmount output value safely
+                if (pTotalAmount.Value != DBNull.Value)
+                {
+                    outObj.ExtraData["TotalAmount"] = Convert.ToInt32(pTotalAmount.Value);
+                }
+
+                // ✅ Apply pagination only if both values are set
+                if (pTotalCount != null && pageIndex.HasValue && pageSize.HasValue)
+                {
+                    PaginationHelper.ApplyPagination(outObj, pTotalCount.Value?.ToString(), pageIndex.Value, pageSize.Value);
+                }
+
+                return outObj;
+
+            }, nameof(GetVisitorWorkDetailsList), out _, skipTokenCheck: false));
+
+
+        }
+
 
         [HttpPost("CrudMailMasterDetails")]
         public IActionResult CrudMailMasterDetails([FromBody] object input)
@@ -5640,6 +5920,9 @@ namespace Jabalpur_Office.Controllers
                 return outObj;
             }, nameof(GetMailMasterDetails), out _, skipTokenCheck: false));
         }
+
+
+       
 
         [HttpPost("SendSMSService")]
         public async Task<WrapperListData> SendSMSService([FromBody] object input)
