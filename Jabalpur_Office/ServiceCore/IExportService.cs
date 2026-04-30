98c7fde413ddc421233806ed5af553ff9248d966
kirankumaradigoppula <adigoppula.kkumar@gmail.com>
2026-04-30 18:25:50 +0530

    New IExportService

---
+++ Jabalpur_Office/ServiceCore/IExportService.cs
@@ -0,0 +1,13 @@
+﻿using static Jabalpur_Office.Controllers.ZipController;
+using Microsoft.Reporting.WebForms;
+using System.Data;
+
+namespace Jabalpur_Office.ServiceCore
+{
+    public interface IExportService
+    {
+        byte[] ExportSsrs(string reportName, string format, ReportParameter[] parameters);
+
+        byte[] ExportFromDataTable(DataTable dt, string exportType, string fileName, string title, List<ImagePdfItem> images, out string contentType);
+    }
+}
